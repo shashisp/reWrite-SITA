9a2169e38374429db7792537e2c4c1a78281200d
Shashi Kumar P <shashiessp@gmail.com>
2015-02-21 01:51:59 +0530

    Add Docstrings and fix basic model

--- src/application/models.py
+++ src/application/models.py
@@ -8,18 +8,10 @@ App Engine datastore models
 
 from google.appengine.ext import ndb
 
-
-class ExampleModel(ndb.Model):
-    """Example Model"""
-    example_name = ndb.StringProperty(required=True)
-    example_description = ndb.TextProperty(required=True)
+class SchoolModel(ndb.Model):
+    """"Basic Model""""
+    name = ndb.StringProperty(required=True)
+    place = ndb.StringProperty(required=True)
     added_by = ndb.UserProperty()
     timestamp = ndb.DateTimeProperty(auto_now_add=True)
-
-
-class SchoolModel(ndb.Model):
-	name = ndb.StringProperty(required=True)
-	place = ndb.StringProperty(required=True)
-	added_by = ndb.UserProperty()
-	timestamp = ndb.DateTimeProperty(auto_now_add=True)
-	score = ndb.IntegerProperty()
+    score = ndb.IntegerProperty()
